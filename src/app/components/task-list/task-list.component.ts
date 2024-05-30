88c9675ebbbdcdddae17969537f1c92a412c23fa
Adam Lisowski <adam.foxman@outlook.com>
2024-05-30 13:19:26 +0200

    task list fetch mockup

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { AppMaterialModule } from "../../material.module";
 import { MatTableDataSource } from "@angular/material/table";
-import { TaskDto } from "../../clients/api.client";
+import { Priority, TaskClient, TaskDto } from "../../clients/api.client";
 import { DatePipe } from "@angular/common";
 
 @Component({
@@ -14,7 +14,7 @@ import { DatePipe } from "@angular/common";
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
   @Input()
   tasks!: MatTableDataSource<TaskDto>;
 
@@ -24,4 +24,45 @@ export class TaskListComponent {
     'done',
     'priority',
   ]
+
+  constructor(
+    private taskClient: TaskClient,
+  ) {}
+
+  ngOnInit() {
+    this.tasks = new MatTableDataSource<TaskDto>();
+    this.taskClient.getAllTasks().subscribe((tasks) => {
+      this.tasks.data = tasks.tasks;
+    }, (error) => {
+      this.tasks.data = [
+        {
+          id: '1',
+          title: 'Task 1',
+          dueDate: new Date(),
+          done: false,
+          priority: Priority.HIGH,
+          owner: '12345',
+          description: 'This is a test task'
+        },
+        {
+          id: '2',
+          title: 'Task 2',
+          dueDate: new Date(),
+          done: false,
+          priority: Priority.LOW,
+          owner: '12345',
+          description: 'This is a test task'
+        },
+        {
+          id: '3',
+          title: 'Task 3',
+          dueDate: new Date(),
+          done: false,
+          priority: Priority.MEDIUM,
+          owner: '12345',
+          description: 'This is a test task'
+        }
+      ]
+    });
+  }
 }
